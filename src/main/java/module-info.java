b0feadc103f5298eb44b57e138b72e4969ae434b
perebartrolisimo <pere.bartroli@ub.uu.se>
2021-08-20 08:11:13 +0000

    New project cora-testutils

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * The testutils module provides common utils used when testing Cora projects.
+ */
+module se.uu.ub.cora.testutils {
+
+	requires org.testng;
+
+	exports se.uu.ub.cora.testutils.mcr;
+
+}
